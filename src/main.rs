236a18e2e2cfd848217e7bc750ffca4c183cf310
barbieri97 <barbieri97@mail.com>
2022-12-28 15:37:33 -0300

    change the input of text to stdin

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use arboard::Clipboard;
-use std::env;
+// use std::env;
+use std::io;
 
 fn main() {
     let mut clipboard = Clipboard::new().unwrap();
 
-    let key = "GITHUBTOKEN";
-    match env::var(key) {
-        Ok(val) => {
-            clipboard.set_text(&val).unwrap();
-            println!("Token copy to clipboard");
+    let mut buffer = String::new();
+    match io::stdin().read_line(&mut buffer){
+        Ok(n) => {
+            clipboard.set_text(&*buffer.trim()).unwrap();
+            println!("{n} bytes copy to clipboard");
         }
-        Err(e) => println!("couldn't interpret {key}: {e}"),
+        Err(error) => println!("error: {error}"),
     }
+
+    // Implementar a possibilidade de passar uma variavel de ambiente
+    // como argumento para add no clipboard
+    // TODO
+    // let key = "GITHUBTOKEN";
+    // match env::var(key) {
+    //     Ok(val) => {
+    //         clipboard.set_text(&val).unwrap();
+    //         println!("Token copy to clipboard");
+    //     }
+    //     Err(e) => println!("couldn't interpret {key}: {e}"),
+    // }
+
 }
